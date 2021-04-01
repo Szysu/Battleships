dac2e5900318a6f2be39f34574f22a6c70887386
Szysu <szysu1@gmail.com>
2021-04-01 17:18:18 +0200

    Create tests for Shoot method for Game class

--- BattleshipsTests/GameTests.cs
+++ BattleshipsTests/GameTests.cs
@@ -1,4 +1,5 @@
-﻿using Battleships;
+﻿using System;
+using Battleships;
 using Xunit;
 
 namespace BattleshipsTests
@@ -17,6 +18,50 @@ namespace BattleshipsTests
         {
             var game = new Game();
             Assert.NotNull(game);
+        }
+
+        [Fact]
+        public void Shoot_ShipLocationString_ReturnsTrue()
+        {
+            var ship = _game.Ships[0];
+            var shipLocation = ship.Locations[0];
+            var isHit = _game.Shoot(shipLocation);
+            Assert.True(isHit);
+        }
+
+        [Fact]
+        public void Shoot_EmptyLocationString_ReturnsFalse()
+        {
+            string randomLocation;
+
+            do
+            {
+                randomLocation = _game.GetRandomLocation();
+            } while (_game.Ships.Any(ship => ship.Locations.Contains(randomLocation)));
+
+            var isHit = _game.Shoot(randomLocation);
+            Assert.False(isHit);
+        }
+
+        [Theory]
+        [InlineData("A0")]
+        [InlineData("A11")]
+        [InlineData("K1")]
+        [InlineData("K11")]
+        [InlineData("TEST0")]
+        public void Shoot_InvalidLocationString_ThrowsArgumentOutOfRangeException(string location)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _game.Shoot(location));
+        }
+
+        [Fact]
+        public void Shoot_DuplicateShoots_ThrowsArgumentException()
+        {
+            var randomLocation = _game.GetRandomLocation();
+
+            _game.Shoot(randomLocation);
+
+            Assert.Throws<ArgumentException>(() => _game.Shoot(randomLocation));
         }
     }
 }
