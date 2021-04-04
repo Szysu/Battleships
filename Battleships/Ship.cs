690444bd1e896ca2f6de07fe01199cdf9a80d133
Szysu <szysu1@gmail.com>
2021-04-05 01:30:13 +0200

    Refactor tests and logic
    Create 'Location' class to simplify working with coordinates.
    Using strings to store coordinates is good, but extracting it to
    another class improves readability.
    
    Change the way of storing ship locations.
    Two separate lists for storing ship locations and hit locations are
    unnecessary duplication of data.
    A better way is to create a Dictionary with location as a key
    and condition as value.
    
    Adapt 'Game' class to use 'Location' class.
    
    Implement console support in 'Program' class.
    
    Add tests for each newly created method.

--- Battleships/Ship.cs
+++ Battleships/Ship.cs
@@ -1,12 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Battleships
 {
+    /// <summary>
+    ///     A class for storing the ship locations and their condition.
+    /// </summary>
     public class Ship
     {
-        public bool IsSunk => DamagedLocations.Count == Locations.Count;
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Ship"/> class.
+        /// </summary>
+        /// <param name="locations">The ship locations.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     <paramref name="locations"/> is null or empty.
+        /// </exception>
+        public Ship(ICollection<Location> locations)
+        {
+            if (locations == null || !locations.Any())
+                throw new ArgumentNullException(nameof(locations));
 
-        public List<string> Locations { get; set; }
-        public List<string> DamagedLocations { get; set; }
+            Locations = new Dictionary<Location, bool>();
+
+            foreach (var location in locations)
+                Locations.Add(location, false);
+        }
+
+        /// <summary>
+        ///     Indicates whether all locations have been hit.
+        /// </summary>
+        public bool IsSunk => Locations.All(loc => loc.Value);
+
+        /// <summary>
+        ///     A dictionary containing the ship locations and their condition.
+        /// </summary>
+        public Dictionary<Location, bool> Locations { get; }
     }
 }
---
+++ BattleshipsTests/ShipTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Battleships;
+using Xunit;
+
+namespace BattleshipsTests
+{
+    public class ShipTests
+    {
+        private readonly Ship _ship;
+
+        public ShipTests()
+        {
+            var list = new List<Location>
+            {
+                new Location('A', 1),
+                new Location('A', 2),
+                new Location('A', 3),
+                new Location('A', 4)
+            };
+
+            _ship = new Ship(list);
+        }
+
+        [Fact]
+        public void PropertyLocations_ReturnsDictionary()
+        {
+            Assert.IsType<Dictionary<Location, bool>>(_ship.Locations);
+        }
+
+        [Fact]
+        public void PropertyIsSunk_ReturnsCorrectBoolean()
+        {
+            Assert.False(_ship.IsSunk);
+
+            // Sets all ship locations as hit
+            _ship.Locations.Keys.ToList().ForEach(k => _ship.Locations[k] = true);
+
+            Assert.True(_ship.IsSunk);
+        }
+
+        [Fact]
+        public void Constructor_InvalidIEnumerable_ThrowsArgumentNullException()
+        {
+            var list = new List<Location>();
+            Assert.Throws<ArgumentNullException>(() => new Ship(list));
+            Assert.Throws<ArgumentNullException>(() => new Ship(null));
+        }
+    }
+}
