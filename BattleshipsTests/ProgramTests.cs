bd86c7adf89ced73938fb8e08e7f94f9d7014efb
Szysu <szysu1@gmail.com>
2021-04-05 04:49:04 +0200

    Add missing brackets to 'ProgramTests' class

--- BattleshipsTests/ProgramTests.cs
+++ BattleshipsTests/ProgramTests.cs
@@ -43,10 +43,12 @@ namespace BattleshipsTests
             var allLocations = new StringBuilder();
 
             for (var i = 'A'; i <= 'J'; i++)
+            {
                 for (var j = 1; j <= 10; j++)
                 {
                     allLocations.AppendLine($"{i}{j}");
                 }
+            }
 
             return allLocations.ToString();
         }
