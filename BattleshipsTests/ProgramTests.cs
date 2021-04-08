038c9e7dbc45d106e69d95985761e4922f5b83bf
Szysu <szysu1@gmail.com>
2021-04-08 07:22:06 +0200

    Add unit test for 'Main' method in 'Program' class.

--- BattleshipsTests/ProgramTests.cs
+++ BattleshipsTests/ProgramTests.cs
@@ -1,6 +1,45 @@
-﻿namespace BattleshipsTests
+﻿using System;
+using System.IO;
+using System.Text;
+using Battleships;
+using Xunit;
+
+namespace BattleshipsTests
 {
     public class ProgramTests
     {
+        [Fact]
+        public void Main_CorrectlyRunsProgram()
+        {
+            // 'G11' and 'A1' added to check the detection of duplicate shots and invalid inputs.
+            var allLocations = string.Format("G11{0}A1{0}{1}", Environment.NewLine, GetAllLocations());
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            var input = new StringReader(allLocations);
+            Console.SetIn(input);
+
+            Program.Main(Array.Empty<string>());
+
+            Assert.Contains("The entered location is invalid.", output.ToString());
+            Assert.Contains("The entered location has been previously shot!", output.ToString());
+            Assert.Contains("Thank you for playing!", output.ToString());
+        }
+
+        private string GetAllLocations()
+        {
+            var allLocations = new StringBuilder();
+
+            for (var i = 'A'; i <= 'J'; i++)
+            {
+                for (var j = 1; j <= 10; j++)
+                {
+                    allLocations.AppendLine($"{i}{j}");
+                }
+            }
+
+            return allLocations.ToString();
+        }
     }
 }
